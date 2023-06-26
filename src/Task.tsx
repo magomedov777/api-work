34ff832b715c02b15e2125d46534b6f71f63ad58
Arkadiy <alibullterrier@gmail.com>
2023-06-26 05:45:51 +0300

    task type

--- src/Task.tsx
+++ src/Task.tsx
@@ -5,13 +5,7 @@ import IconButton from '@mui/material/IconButton';
 import Checkbox from '@mui/material/Checkbox';
 import { TaskType } from './Todolist'
 
-type TaskPropsType = {
-    task: TaskType
-    todolistId: string
-    changeTaskStatus: (id: string, isDone: boolean, todolistId: string) => void
-    changeTaskTitle: (taskId: string, newTitle: string, todolistId: string) => void
-    removeTask: (taskId: string, todolistId: string) => void
-}
+
 export const Task = React.memo((props: TaskPropsType) => {
     const onClickHandler = useCallback(() => props.removeTask(props.task.id, props.todolistId), [props.task.id, props.todolistId]);
 
