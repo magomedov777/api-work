9742ed40bdc6fd0f10225b1b936a760c4b4ec94d
Arkadiy <alibullterrier@gmail.com>
2023-06-26 05:42:34 +0300

    api-working

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -7,11 +7,7 @@ import { Delete } from '@mui/icons-material';
 import { Task } from './Task'
 import { FilterValuesType } from './App';
 
-export type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
-}
+
 
 type PropsType = {
     id: string
