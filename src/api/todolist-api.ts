0696bd4c695d06c1e406d247a87a21c04bf59129
Arkadiy <alibullterrier@gmail.com>
2023-06-27 06:58:00 +0300

    rm

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -6,18 +6,10 @@ const instance = axios.create({
 });
 
 export const TodolistApi = {
-  getTodolists() {
-    return instance.get<TodolistType[]>("todo-lists");
-  },
-  createTodolists(title: string) {
-    return instance.post<ResponseType<{item: TodolistType}>>("todo-lists", { title });
-  },
-  deleteTodolists(id: string) {
-    return instance.delete<ResponseType>(`todo-lists/${id}`);
-  },
-  updateTodolists(id: string, title: string) {
-    return instance.put<ResponseType>(`todo-lists/${id}`, { title });
-  },
+  
+ 
+  
+  
 };
 
 type TodolistType = {
