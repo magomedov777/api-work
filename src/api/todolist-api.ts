ebdb724609bc514ee60d7f35c9fd2a3b31e96731
Arkadiy <alibullterrier@gmail.com>
2023-07-25 06:13:21 +0300

    get todos

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -50,7 +50,6 @@ type TasksResponseType = {
 
 export const TodolistApi = {
   getTodolists() {
-    return instance.get<TodolistType[]>("todo-lists");
   },
   createTodolists(title: string) {
     return instance.post<ResponseType<{item: TodolistType}>>("todo-lists", { title });
