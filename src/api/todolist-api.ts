e29a91703fc880573d65c150e4325004d10f7062
Arkadiy <alibullterrier@gmail.com>
2023-08-01 04:57:01 +0300

    create instance

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-
+const instance = axios.create({
+  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  withCredentials: true,
+});
 
 type TodolistType = {
   id: string
