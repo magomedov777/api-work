1d7cadaa556ec6b140061d386c687608a9865827
Arkadiy <alibullterrier@gmail.com>
2023-08-01 04:56:42 +0300

    add base URL

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1/",
-  withCredentials: true,
-});
+
 
 type TodolistType = {
   id: string
